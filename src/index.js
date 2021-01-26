2c9b4bd099df83d818bc0a73f248a7d12678870b
otto <otto@elfo.com>
2021-01-26 17:54:50 +0800

    shooting works

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import speakerImg from './assets/speaker.png';
 // import theWeekend from './assets/theWeekend.mp3';
 import shipImg from './assets/ship.png';
 import beamShot1 from './assets/beamShot1.png';
-// import shoot from './shoot.js'
+import Shoot from './shoot.js'
 
 var shipWorld = {
     velocity: 8
@@ -21,31 +21,18 @@ class MyGame extends Phaser.Scene {
     }
 
     preload() {
-        // this.load.image('logo', logoImg);
-        // this.load.image('space', spaceImg);
         this.load.image('spaceBG', spaceImgBG);
 
-        // load the music
-        this.load.image('speaker', speakerImg);
-        // this.load.audio('loading', theWeekend);
         this.load.image('ship', shipImg);
-        this.load.image('beamShot1', beamShot1);
+        // this.load.image('beamShot1', beamShot1);
 
         // Add key input to the game
         this.keys = this.input.keyboard.createCursorKeys();
-        // this.load.image('shoot', shoot);
+        this.load.image('shoot', beamShot1);
     }
 
     create() {
         this.add.image(0, 0, 'spaceBG').setOrigin(0, 0).setScale(1);
-
-        // this.tweens.add({
-        //     targets: space,
-        //     x: 450,
-        //     ease: "Power2",
-        //     yoyo: true,
-        //     loop: -1
-        // });
 
         this.add.text(280, 350, `Otto`, {
             fontSize: '32px',
@@ -57,28 +44,21 @@ class MyGame extends Phaser.Scene {
             fill: '#FF0000',
             fontStyle: 'bold',
         }).setInteractive({ useHandCursor: true }).on('pointerdown', () => {
-            // music.play()
-            // return this.scene.start('PlayScene');
-            music.play()
+
             this.add.image(100, 400, 'ship').setScale(0.6);
             // this.physics.add.image(400, 300, 'ship').setScale(0.1);
         });
 
-        // load the music 
-        // this.add.image(780, 580, 'speaker').setScale(.1).setInteractive({ useHandCursor: true }).on('pointerdown', () => music.play());
-
-        // const music = this.sound.add('loading', {
-        //     mute: false,
-        //     volume: 0.15,
-        //     rate: 1,
-        //     detune: 0,
-        //     seek: 0,
-        //     loop: true,
-        //     delay: 0,
-        // })
-        // this.add.image(780, 580, 'speaker').setScale(.1).setInteractive({ useHandCursor: true }).on('pointerdown', () => music.play());
-
         this.ship = this.add.image(100, 400, 'ship').setScale(0.6);
+
+        // shoot 
+        this.shootsGroup = this.physics.add.group({
+            classType: Shoot,
+            maxSize: 1,
+            runChildUpdate: true,
+        });
+
+        // this.physics.add.overlap(this.shootsGroup, this.asteroidsGroup, this.collision, null, this);
     }
     update() {
         // Poll the arrow keys to move the ship
@@ -96,12 +76,10 @@ class MyGame extends Phaser.Scene {
         }
 
         if (this.keys.space.isDown) {
-            // this.beamShot1 = this.add.image(this.ship.x, this.ship.y, 'beamShot1')
-            // this.beamShot1.x += shipWorld.velocity
-            // for (let index = 0; index < 30; index++) {
-            //     this.beamShot1.x += bullet.velocity + index
-            //     index++
-            // }
+            const shoot = this.shootsGroup.get();
+            if (shoot) {
+                shoot.fire(this.ship.x, this.ship.y, this.ship.rotation);
+            }
         }
     }
 
@@ -110,11 +88,30 @@ class MyGame extends Phaser.Scene {
 
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
+    scene: MyGame,
     width: 800,
     height: 600,
-    scene: MyGame
+    type: Phaser.AUTO,
+    audio: {
+        disableWebAudio: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            fps: 60,
+            gravity: { y: 0 },
+        }
+    },
 };
 
 const game = new Phaser.Game(config);
+
+// const config = {
+//     type: Phaser.AUTO,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     scene: MyGame
+// };
+
+// const game = new Phaser.Game(config);
